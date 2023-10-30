574a223d8ee78810ee3ec53ecb1dc69c4c169f39
imamwahyu635 <imamwahyu635@gmail.com>
2023-10-30 19:06:08 +0700

    firts commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import express, { json } from "express";
+import { exec } from "child_process";
+import cors from "cors"
+const app = new express();
+const port = 3000;
+app.use(cors())
+app.use(express.json())
+
+
+const clonerepo = "git clone https://github.com/hajilok/youtube-live-streaming && cd youtube-live-streaming"
+const install = "npm install"
+const build = "pm2 main.js"
+
+const execshell = (shell) => {
+    exec(shell, (err, stdout, stderr) => {
+        if (err) {
+            res.send(`error: ${err.message}`);
+            return;
+        }
+    })
+}
+
+
+
+
+
+app.get("/api/deploy/:key", async (req, res) => {
+    const key = req.params.key;
+    const getkey = `echo streamkey=${key} > .env`
+
+    execshell(clonerepo)
+    execshell(getkey)
+    execshell(install)
+    execshell(build)
+
+
+
+
+
+    res.status(200).send({ status: "success", data: handle });
+
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
